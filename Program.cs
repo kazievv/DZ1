563fb5c7a8e5ea9d3067f5f8a4ae62382e5c50f0
Kaziev <kazie_v15@mail.ru>
2023-05-18 16:08:27 +0300

    Repost

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 1");
+Console.Write("Введите число");
+string? number = Console.ReadLine();
+
+void CheckingNumber (string number){
+    if (number[0] == number[4] || number[1] == number[3]){
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром."); 
+}
+
+if (number!.Length == 5){
+    CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число:");
